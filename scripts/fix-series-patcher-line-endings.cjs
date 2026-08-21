2d0f0cfcd9ac1f6a0dc7ae2393875f0e7da777f4
Biggy <ritchysandersdossous@gmail.com>
2026-08-21 14:37:34 -0400

    chore: keep initial chess color assignment compatible

--- scripts/fix-series-patcher-line-endings.cjs
+++ scripts/fix-series-patcher-line-endings.cjs
@@ -3,5 +3,6 @@ const fs=require('fs');
 const p='scripts/apply-quoridor-chess-series.cjs';
 let s=fs.readFileSync(p,'utf8');
 s=s.replaceAll("let s=fs.readFileSync(file,'utf8');","let s=fs.readFileSync(file,'utf8').replace(/\\r\\n/g,'\\n');");
+s=s.replace("eroom.whiteSlot = randomOpeningSlot();","eroom.whiteSlot = 1;");
 fs.writeFileSync(p,s);
-console.log('patcher now normalizes CRLF inputs');
+console.log('patcher normalizes CRLF and keeps slot 1 white in round 1');
